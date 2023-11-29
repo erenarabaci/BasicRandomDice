b885b4f0e36b51e8431787839f86d54d98c76e87
Eren Arabacı <erenarabaci@windowslive.com>
2023-11-29 10:07:19 +0300

    last save

---
+++ main.py
@@ -0,0 +1,44 @@
+import tkinter as tk
+from tkinter import Label, Button
+from PIL import Image, ImageTk  
+import random
+
+class ZarAtmaOyunu:
+    def __init__(self, master):
+        self.master = master
+        self.master.title("Zar Atma Oyunu")
+
+        
+        self.zar_resmi = ImageTk.PhotoImage(Image.open("zar_resmi.png"))  #
+        self.zar_etiket = Label(self.master, image=self.zar_resmi)
+        self.zar_etiket.pack(pady=20)
+
+        # Zar atma düğmesi
+        self.zar_atma_dugme = Button(self.master, text="Zar At", command=self.zar_at)
+        self.zar_atma_dugme.pack()
+
+    def zar_at(self):
+        donme = random.randint(1,32)
+        for i in range(donme):
+            zar_sayisi = random.randint(1, 6)
+            zar_resmi_adi = f"zar{zar_sayisi}.png"
+            self.zar_resmi = ImageTk.PhotoImage(Image.open(zar_resmi_adi))
+            self.zar_etiket.configure(image=self.zar_resmi)
+            self.master.update()
+            self.master.after(120)
+
+        
+       
+        self.zar_resmi = ImageTk.PhotoImage(Image.open(zar_resmi_adi))
+
+        
+        self.zar_etiket.configure(image=self.zar_resmi)
+
+
+root = tk.Tk()
+
+
+zar_oyunu = ZarAtmaOyunu(root)
+
+
+root.mainloop()
